10a24a5b7bf8c7de263f3d11141932f0c6384166
prateekpandey14 <prateek.pandey@mayadata.io>
2020-04-28 23:18:20 +0530

    fix(deps): remove indirect version dependency to openebs/api
    Signed-off-by: prateekpandey14 <prateek.pandey@mayadata.io>

---
+++ pkg/version/util.go
@@ -0,0 +1,24 @@
+package version
+
+import (
+	"strings"
+)
+
+var (
+	validCurrentVersions = map[string]bool{
+		"1.9.0": true, "1.10.0": true,
+	}
+	validDesiredVersion = GetVersion()
+)
+
+// IsCurrentVersionValid verifies if the  current version is valid or not
+func IsCurrentVersionValid(v string) bool {
+	currentVersion := strings.Split(v, "-")[0]
+	return validCurrentVersions[currentVersion]
+}
+
+// IsDesiredVersionValid verifies the desired version is valid or not
+func IsDesiredVersionValid(v string) bool {
+	desiredVersion := strings.Split(v, "-")[0]
+	return validDesiredVersion == desiredVersion
+}

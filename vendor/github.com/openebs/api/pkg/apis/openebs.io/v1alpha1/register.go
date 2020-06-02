9d87aef3c1b3570924f2137777a062c2209eef8f
mittachaitu <sai.chaithanya@mayadata.io>
2020-06-02 21:08:01 +0530

    This commit updates the go.mod, go.sum and vendor files
    Signed-off-by: mittachaitu <sai.chaithanya@mayadata.io>

--- vendor/github.com/openebs/api/pkg/apis/openebs.io/v1alpha1/register.go
+++ vendor/github.com/openebs/api/pkg/apis/openebs.io/v1alpha1/register.go
@@ -58,6 +58,12 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&BlockDeviceList{},
 		&BlockDeviceClaim{},
 		&BlockDeviceClaimList{},
+		&CStorBackup{},
+		&CStorBackupList{},
+		&CStorCompletedBackup{},
+		&CStorCompletedBackupList{},
+		&CStorRestore{},
+		&CStorRestoreList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil

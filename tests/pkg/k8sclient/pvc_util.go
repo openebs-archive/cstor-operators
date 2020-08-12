0758d380728862c096619663fb85f77493d7a52b
sai chaithanya <sai.chaithanya@mayadata.io>
2020-08-13 00:31:47 +0530

    feat(tests): add integration test cases for changing the policies on CVC dynamically (#148)
    feat(tests): add integration test cases for changing the policies on CVC dynamically
    
    This PR adds covers the following integration test cases:
    - Updating (includes adding and removing) resource requirements on CVC.
    - Adding toleration on CVC.
    - Adding priority class name on CVC.
    - Update luworkers and queuedepth values on CVC.
    - Updating CVC with a target node selector.
    
    Signed-off-by: mittachaitu <sai.chaithanya@mayadata.io>

--- tests/pkg/k8sclient/pvc_util.go
+++ tests/pkg/k8sclient/pvc_util.go
@@ -62,7 +62,7 @@ func (client *Client) WaitForPersistentVolumeClaimPhase(
 		if pvcObj.Status.Phase == expectedPhase {
 			return nil
 		}
-		klog.Infof("PersistentVolumeClaim %s found and phase=%s (%v)", pvcName, expectedPhase, time.Since(start))
+		klog.Infof("PersistentVolumeClaim %s found and phase=%s (%v)", pvcName, pvcObj.Status.Phase, time.Since(start))
 	}
 	return errors.Errorf("PersistentVolumeClaim %s not at all in phase %s", pvcName, expectedPhase)
 }
@@ -72,9 +72,9 @@ func (client *Client) GetPVC(pvcName, pvcNamespace string) (*corev1.PersistentVo
 	return client.KubeClientSet.CoreV1().PersistentVolumeClaims(pvcNamespace).Get(pvcName, metav1.GetOptions{})
 }
 
-// WaitForPersistentVolumeClaimDeleted waits for a PersistentVolumeClaim
+// WaitForPersistentVolumeClaimDeletion waits for a PersistentVolumeClaim
 // to be removed from the system until timeout occurs, whichever comes first
-func (client *Client) WaitForPersistentVolumeClaimDeleted(pvcName, pvcNamespace string, poll, timeout time.Duration) error {
+func (client *Client) WaitForPersistentVolumeClaimDeletion(pvcName, pvcNamespace string, poll, timeout time.Duration) error {
 	for start := time.Now(); time.Since(start) < timeout; time.Sleep(poll) {
 		_, err := client.GetPVC(pvcName, pvcNamespace)
 		if err != nil {

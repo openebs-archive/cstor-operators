a185db965da2c1fbcfbbee9a1f54bcfe9784ed44
Akhil Mohan <akhil.mohan@mayadata.io>
2021-01-11 20:21:30 +0530

    add image pull secrets to pool and volume deployment
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- pkg/cspc/algorithm/build_deploy.go
+++ pkg/cspc/algorithm/build_deploy.go
@@ -100,6 +100,7 @@ func (c *Config) GetPoolDeploySpec(cspi *cstor.CStorPoolInstance) *appsv1.Deploy
 				WithAnnotationsNew(getPodAnnotations()).
 				WithServiceAccountName(util.GetServiceAccountName()).
 				WithTolerations(getPoolPodToleration(cspi)...).
+				WithImagePullSecrets(getImagePullSecrets(getOpenEBSImagePullSecrets())).
 				WithContainers(
 					coreapi.NewContainer().
 						WithImage(getPoolMgmtImage()).

54ff4f63e8fc46f46869b998e1573c7c133e74d8
shubham <shubham.bajpai@mayadata.io>
2021-04-21 18:06:53 +0530

    refact(actions): use build & push action instead of makefile   - add quay.io repo to push images   - avoid build when release is created
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>

--- Makefile.buildx.mk
+++ Makefile.buildx.mk
@@ -21,8 +21,6 @@
 
 export DBUILD_ARGS=--build-arg DBUILD_DATE=${DBUILD_DATE} --build-arg DBUILD_REPO_URL=${DBUILD_REPO_URL} --build-arg DBUILD_SITE_URL=${DBUILD_SITE_URL} --build-arg RELEASE_TAG=${RELEASE_TAG} --build-arg BRANCH=${BRANCH}
 
-CSTOR_BASE_IMAGE= ${IMAGE_ORG}/cstor-base:${BASE_TAG}
-
 ifeq (${TAG}, )
   export TAG=ci
 endif
